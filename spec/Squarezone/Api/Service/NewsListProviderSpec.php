38ed4d7d90af7c83a95cccfbc7a6baad2c99d119
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2015-11-17 23:53:29 +0100

    spec for news list provider service

--- spec/Squarezone/Api/Service/NewsListProviderSpec.php
+++ spec/Squarezone/Api/Service/NewsListProviderSpec.php
@@ -2,8 +2,10 @@
 
 namespace spec\Squarezone\Api\Service;
 
+use Doctrine\DBAL\Connection;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\HttpFoundation\Request;
 
 class NewsListProviderSpec extends ObjectBehavior
 {
@@ -12,5 +14,49 @@ class NewsListProviderSpec extends ObjectBehavior
         $this->shouldHaveType('Squarezone\Api\Service\NewsListProvider');
     }
 
-    // TODO: implement tests
+    function it_provides_news_list(Request $request, Connection $db)
+    {
+        $db->fetchAll('SELECT id_news, title, slug, creation_date FROM news LIMIT 0,25')->willReturn(range(1, 15));
+
+        $this->get($request, $db)->shouldHaveCount(15);
+    }
+
+    function it_provides_news_list_for_specific_year(Request $request, Connection $db)
+    {
+        $request->get('year', false)->willReturn('2015');
+        $request->get('month', false)->willReturn(false);
+        $request->get('day', false)->willReturn(false);
+        $request->get('slug', false)->willReturn(false);
+
+        $page = $req->get('page', 1)->willReturn(false);
+        $size = $req->get('size', 25)->willReturn(false);
+
+        $db->fetchAll('SELECT id_news, title, slug, creation_date FROM news WHERE YEAR(creation_date)="2015"')->willReturn(range(1, 15));
+
+        $this->get($request, $db)->shouldHaveCount(15);
+    }
+
+    function it_provides_news_list_for_specific_year_and_month(Request $request, Connection $db)
+    {
+        $request->get('year', false)->willReturn('2015');
+        $request->get('month', false)->willReturn('01');
+        $request->get('day', false)->willReturn(false);
+        $request->get('slug', false)->willReturn(false);
+
+        $db->fetchAll('SELECT id_news, title, slug, creation_date FROM news WHERE YEAR(creation_date)="2015" && MONTH(creation_date)="01"')->willReturn(range(1, 15));
+
+        $this->get($request, $db)->shouldHaveCount(15);
+    }
+
+    function it_provides_news_list_for_specific_year_month_day_and_slug(Request $request, Connection $db)
+    {
+        $request->get('year', false)->willReturn('2015');
+        $request->get('month', false)->willReturn('01');
+        $request->get('day', false)->willReturn('01');
+        $request->get('slug', false)->willReturn('test');
+
+        $db->fetchAll('SELECT id_news, title, slug, creation_date FROM news WHERE YEAR(creation_date)="2015" && MONTH(creation_date)="01" && DAY(creation_date)="01" && slug="test"')->willReturn(range(1, 15));
+
+        $this->get($request, $db)->shouldHaveCount(15);
+    }
 }
