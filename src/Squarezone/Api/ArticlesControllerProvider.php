6c23ef3a5bfd1df9b5ba02966834e5664aa99751
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2015-11-17 01:18:15 +0100

    articles for a category

--- src/Squarezone/Api/ArticlesControllerProvider.php
+++ src/Squarezone/Api/ArticlesControllerProvider.php
@@ -2,11 +2,15 @@
 
 namespace Squarezone\Api;
 
+use Doctrine\DBAL\Connection;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticlesControllerProvider implements ControllerProviderInterface {
+
+    const SIZE = 25;
 
     /**
      * Returns routes to connect to the given application.
@@ -19,60 +23,164 @@ class ArticlesControllerProvider implements ControllerProviderInterface {
     {
         $controllers = $app['controllers_factory'];
 
-        // TODO: Implement connect() method.
-        $controllers->get('/articles', function() use ($app) {
-            $articles = $app['db']->fetchAll('SELECT id_article, title, slug, creation_date FROM article LIMIT 0,30');
+        $controllers->get('/articles', function(Request $req) use ($app) {
+            $year = $req->get('year', false);
+            $month = $req->get('month', false);
+            $day = $req->get('day', false);
+            $slug = $req->get('slug', false);
 
-            $url = $app['host'] . '/index.php';
+            if ($year) {
+                $sql = 'SELECT id_news, title, slug, creation_date FROM news WHERE YEAR(creation_date)="'.$year.'"';
+                if ($month) {
+                    $sql = 'SELECT id_news, title, slug, creation_date FROM news WHERE YEAR(creation_date)="'.$year.'" && MONTH(creation_date)="'.$month.'"';
+                    if ($day && $slug) {
+                        $sql = 'SELECT id_news, title, slug, creation_date FROM news WHERE YEAR(creation_date)="'.$year.'" && MONTH(creation_date)="'.$month.'" && DAY(creation_date)="'.$day.'" && slug="'.$slug.'"';
+                    }
+                }
+            } else {
+                $sql = 'SELECT id_news, title, slug, creation_date FROM news LIMIT 0,25';
+            }
 
-            $api = array(
-                'links' => array(
-                    array(
-                        'rel' => 'next',
-                        'href' => $url . '/articles?page=1&size=30'
+            if ($sql) {
+                $news = $app['db']->fetchAll($sql);
+
+
+                $url = $app['host'] . '/index.php';
+
+                foreach ($news as &$item) {
+                    $item['links'] = array(
+                        array(
+                            'rel' => 'self',
+                            'href' => $url . '/news/'.str_replace('-', '/', substr($item['creation_date'], 0, 10)).'/'.$item['slug']
+                        )
+                    );
+                }
+
+                $api = array(
+                    'links' => array(
+                        array(
+                            'rel' => 'next',
+                            'href' => $url . '/news?page=1&size=25'
+                        ),
+                        // array(
+                        //  'rel' => 'self',
+                        //  'href' => $url . '/news{?page,size,sort}'
+                        // )
                     ),
-                    // array(
-                    // 	'rel' => 'self',
-                    // 	'href' => $url . '/news{?page,size,sort}'
-                    // )
-                ),
-                'content' => $articles,
-                'page' => array(
-                    'size' => 30,
-                    'totalElements' => '???',
-                    'totalPages' => '???',
-                    'number' => 0
-                )
-            );
+                    'content' => $news,
+                    'page' => array(
+                        'size' => 25,
+                        'totalElements' => count($news),
+                        'totalPages' => '???',
+                        'number' => 0
+                    )
+                );
 
-            return json_encode($api);
+                return json_encode($api);
+            }
         });
 
-        $controllers->get('/articles/{id}', function($id) use ($app){
-            $sql = "SELECT * FROM article WHERE id_article = ?";
-            //$sql = "SELECT id_news, title, slug FROM news WHERE id_news = ?";
+        $controllers->get('/articles/{category}', function(Request $req) use ($app) {
+            $category = $req->get('category', false);
+
+            if ($category) {
+                $sql = 'SELECT a.id_article, a.title, a.slug, a.creation_date FROM article a LEFT JOIN article_category ac ON(ac.id_article_category=a.id_article_category) WHERE ac.slug="'.$category.'"';
+            }
+
+            $page = $req->get('page', 1);
+            $size = $req->get('size', self::SIZE);
+
+            if ($page) {
+                $sql .= ' LIMIT ' . (($page-1)*$size) . ',' . ($page*$size);
+            }
+
+            if ($sql) {
+                $items = $app['db']->fetchAll($sql);
+
+                $url = $app['host'] . '/index.php';
+
+                foreach ($items as &$item) {
+                    $item['links'] = array(
+                        array(
+                            'rel' => 'self',
+                            'href' => $url . '/articles/'.$category.'/'.$item['slug']
+                        )
+                    );
+                }
+
+                $api = array(
+                    'links' => array(
+                        array(
+                            'rel' => 'next',
+                            'href' => $url . '/articles?page=1&size=25'
+                        ),
+                        // array(
+                        //  'rel' => 'self',
+                        //  'href' => $url . '/news{?page,size,sort}'
+                        // )
+                    ),
+                    'content' => $items,
+                    'page' => array(
+                        'size' => $size,
+                        'totalElements' => count($items),
+                        'totalPages' => '???',
+                        'number' => $page
+                    )
+                );
+
+                return json_encode($api);
+            }
+        })->assert('slug', '\w+');
+
+        $controllers->get('/news/{id}', function($id) use ($app){
+            $sql = "SELECT * FROM news WHERE id_news = ?";
             $post = $app['db']->fetchAssoc($sql, array((int) $id));
 
-            // return  "<h1>{$post['title']}</h1>".
-                    // "<div>{$post['markup']}</div>";
             $url = $app['host'] . '/index.php';
 
             $api = array(
                 'links' => array(
                     array(
                         'rel' => 'self',
-                        'href' => $url . '/articles/' . $id
+                        'href' => $url . '/news/' . $id
                     )
                 ),
-                'articles' => $post
+                'news' => $post
             );
 
             return json_encode($api);
-        })->assert('id', '\d+');
+        })->assert('id', '[a-z-]+');
 
-        // create new article
-        $controllers->post('/article', function() use ($app) {
+
+        $controllers->post('/news', function(Request $req) use ($app) {
+            $token = $req->get('token', false);
+
+            if ($token !== self::TOKEN) {
+                throw new \Exception("Invalid token", 403);
+            }
+
+            /** @var Connection $db */
+            $db = $app['db'];
+
+            $fields = $req->request->all();
+
+            // create slug
+
+            if (empty($req->get('title'))) {
+                throw new \Exception('Missing title', 400);
+            }
+
+            $db->insert('news', $fields);
+
+            $last_id = $db->lastInsertId();
+
             
+
+//            $news = $app['db']->('INSERT INTO news VALUE(id_news, title, slug, creation_date');
+
+            $sql = "SELECT * FROM news WHERE id_news = ?";
+            $news = $db->fetchAssoc($sql, array((int) $last_id));
+
             $url = $app['host'] . '/index.php';
 
             $api = array(
@@ -80,9 +188,19 @@ class ArticlesControllerProvider implements ControllerProviderInterface {
                     array(
                         'rel' => 'next',
                         'href' => $url . '/news?page=1&size=30'
-                    )
+                    ),
+                    // array(
+                    //  'rel' => 'self',
+                    //  'href' => $url . '/news{?page,size,sort}'
+                    // )
                 ),
-                'content' => '$article'
+                'content' => $news,
+                'page' => array(
+                    'size' => 30,
+                    'totalElements' => '???',
+                    'totalPages' => '???',
+                    'number' => 0
+                )
             );
 
             return json_encode($api);
