ef16b07fbc8ebe46c6c27465080783ea72ed29c4
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2015-11-20 22:44:48 +0100

    article provider spec and code

---
+++ spec/Squarezone/Api/Service/ArticleProviderSpec.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace spec\Squarezone\Api\Service;
+
+use Doctrine\DBAL\Connection;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\HttpFoundation\Request;
+
+use Squarezone\SquarezoneException;
+
+class ArticleProviderSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Squarezone\Api\Service\ArticleProvider');
+    }
+
+    function it_provides_article(Request $request, Connection $db)
+    {
+    	$request->get('category', false)->willReturn('crono-trigger');
+    	$request->get('slug', false)->willReturn('recenzja');
+
+        $db->fetchAssoc('SELECT a.id_article, a.title, a.slug, a.creation_date, ac.slug AS category FROM article a LEFT JOIN article_category ac ON(ac.id_article_category=a.id_article_category) WHERE ac.slug="crono-trigger" AND a.slug="recenzja"')->willReturn(array('title' => '', 'slug' => ''));
+
+        $article = $this->get($request, $db);
+
+        $article->shouldHaveKey('title');
+        $article->shouldHaveKey('slug');
+    }
+
+    function it_throws_exception_when_params_incorrect(Request $request, Connection $db)
+    {
+    	$request->get('category', false)->willReturn(false);
+    	$request->get('slug', false)->willReturn(false);
+
+    	$db->fetchAssoc(Argument::type('string'))->shouldNotBeCalled();
+
+    	$this->shouldThrow(SquarezoneException::class)->during('get', array($request, $db));
+    }
+
+    function it_returns_empty_array_when_article_does_not_exists(Request $request, Connection $db)
+    {
+    	$request->get('category', false)->willReturn('aaa');
+    	$request->get('slug', false)->willReturn('bbb');
+
+        $db->fetchAssoc('SELECT a.id_article, a.title, a.slug, a.creation_date, ac.slug AS category FROM article a LEFT JOIN article_category ac ON(ac.id_article_category=a.id_article_category) WHERE ac.slug="aaa" AND a.slug="bbb"')->willReturn(false);
+
+        $this->get($request, $db)->shouldBe(false);
+    }
+}
---
+++ src/Squarezone/Api/Service/ArticleProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Squarezone\Api\Service;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\Request;
+
+use Squarezone\SquarezoneException;
+
+class ArticleProvider
+{
+    public function get(Request $req, Connection $db)
+    {
+        $category = $req->get('category', false);
+        $slug = $req->get('slug', false);
+
+        if (empty($category) || empty($slug)) {
+        	throw new SquarezoneException();
+        }
+
+        $sql = 'SELECT a.id_article, a.title, a.slug, a.creation_date, ac.slug AS category FROM article a LEFT JOIN article_category ac ON(ac.id_article_category=a.id_article_category)';
+
+        $whereParts = [];
+
+        $whereParts[] = sprintf('ac.slug="%s"', $category);
+        $whereParts[] = sprintf('a.slug="%s"', $slug);
+
+        $sql .= ' WHERE ' . implode(' AND ', $whereParts);
+
+        if ($result = $db->fetchAssoc($sql)) {
+        	return $result;
+        }
+        return false;
+    }
+}
