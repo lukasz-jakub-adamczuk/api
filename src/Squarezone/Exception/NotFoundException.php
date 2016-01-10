0c2bc67e2b2d34d05edd431c7356c5605792e75c
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2016-01-10 23:50:05 +0100

    new Exception for Not Found rows in DB

---
+++ src/Squarezone/Exception/NotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Squarezone\Exception;
+
+/**
+ * Created by PhpStorm.
+ * User: ash
+ * Date: 05/11/15
+ * Time: 13:53
+ */
+class NotFoundException extends \Exception
+{
+    
+}
