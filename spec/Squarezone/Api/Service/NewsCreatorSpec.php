787dbcc35902cb9392f7fb04090b4f12efdde966
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2016-01-28 18:23:34 +0100

    specs for news creator and editor

---
+++ spec/Squarezone/Api/Service/NewsCreatorSpec.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace spec\Squarezone\Api\Service;
+
+use Doctrine\DBAL\Connection;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+use Squarezone\Exception\SquarezoneException;
+
+class NewsCreatorSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Squarezone\Api\Service\NewsCreator');
+    }
+
+    function it_creates_news(Connection $db) {
+        $fields = array('title' => 'Potencjalny news');
+        
+        $db->insert('news', Argument::type('array'))->shouldBeCalled();
+        $db->lastInsertId()->willReturn(123);
+
+        $sql = 'SELECT n.id_news, n.title, n.slug, n.creation_date
+                FROM news n 
+                WHERE id_news = ?';
+
+        $db->fetchAssoc($sql, array('123'))->willReturn(array('title' => 'Jakis news'));
+
+        $response = $this->create($fields, $db);
+
+        $response->shouldHaveKey('title');
+    }
+
+    function it_throws_exception_when_title_is_missing(Connection $db) {
+        $this->shouldThrow(SquarezoneException::class)->during('create', array(array(), $db));
+    }
+}
---
+++ spec/Squarezone/Api/Service/NewsEditorSpec.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace spec\Squarezone\Api\Service;
+
+use Doctrine\DBAL\Connection;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+use Squarezone\Exception\SquarezoneException;
+
+class NewsEditorSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Squarezone\Api\Service\NewsEditor');
+    }
+
+    function it_updates_news(Connection $db)
+    {
+        $fields = array('id' => '123', 'title' => 'do zmiany');
+
+        $db->update('news', $fields, array('id_news' => $fields['id']))->shouldBeCalled();
+
+        $sql = 'SELECT *
+                FROM news n 
+                WHERE id_news = ?';
+
+        $db->fetchAssoc($sql, array('123'))->willReturn(array('title' => 'do zmiany'));
+        
+        $response = $this->update($fields, $db);
+
+        $response->shouldHaveKey('title');  
+    }
+
+    function it_throws_exception_when_title_is_missing(Connection $db)
+    {
+        $this->shouldThrow(SquarezoneException::class)->during('update', array(array(), $db));
+    }
+}
